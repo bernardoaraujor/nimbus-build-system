921d46bdb6ef322404fc3ffe9a26cc3a3efa5c68
Zahary Karadjov <zahary@gmail.com>
2019-09-08 13:20:20 -0400

    Another more comprehensive fix

--- scripts/env.sh
+++ scripts/env.sh
@@ -11,8 +11,16 @@
 ## get native Windows paths on Mingw
 #uname | grep -qi mingw && PWD_CMD="pwd -W"
 
+# https://unix.stackexchange.com/a/351658
+if test -n "$BASH" ; then script=$BASH_SOURCE
+elif test -n "$TMOUT"; then script=${.sh.file}
+elif test -n "$ZSH_NAME" ; then script=${(%):-%x}
+elif test ${0##*/} = dash; then x=$(lsof -p $$ -Fn0 | tail -1); script=${x#n}
+else script=$0
+fi
+
 # https://unix.stackexchange.com/a/76518
-export REL_PATH=`dirname "$0"`
+export REL_PATH=`dirname "$script"`
 export ABS_PATH=`exec 2>/dev/null;(cd -- "$REL_PATH") && cd -- "$REL_PATH"|| cd "$REL_PATH"; unset PWD; /usr/bin/pwd || /bin/pwd || pwd`
 
 # do we still need this?
