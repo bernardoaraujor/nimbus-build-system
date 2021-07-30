818df0f01961f90316a3dca849eb8a4afcb23aac
Bernardo A. Rodrigues <barodrigues@protonmail.com>
2021-07-29 23:31:08 -0300

    OE conditional create_nimble_link.sh

--- scripts/create_nimble_link.sh
+++ scripts/create_nimble_link.sh
@@ -13,7 +13,11 @@ module_name="${1#*/}"
 
 if [ `ls -1 *.nimble 2>/dev/null | wc -l ` -gt 0 ]; then
   mkdir -p "${NIMBLE_DIR}/pkgs/${module_name}-#head"
-  PKG_DIR="$(${PWD_CMD})"
+  if [ -n $OE ];then
+    PKG_DIR="$(pwd)"
+  else
+    PKG_DIR="$(${PWD_CMD})"
+  fi
   if [ -d src ]; then
     PKG_DIR="${PKG_DIR}/src"
   fi
